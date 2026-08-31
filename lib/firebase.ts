ed1b819fd9b2e882e307adf8370043200b900271
simonkomimonko603-ctrl <vas-email@example.com>
2026-08-31 07:32:21 +0200

    Fix lib folder location

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCdSyggAlKW-q_pDMwFvLKV1mKYmANA7U",
+  authDomain: "apexpos-ef6a0.firebaseapp.com",
+  projectId: "apexpos-ef6a0",
+  storageBucket: "apexpos-ef6a0.appspot.com",
+  messagingSenderId: "123316014625",
+  appId: "1:123316014625:web:edd04e73ba2d1d524f5d60"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
